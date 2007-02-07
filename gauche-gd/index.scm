ed937f4cdd89874f79fffea79686bf37a134f1d8
Takeshi Abe <tabe@fixedpoint.jp>
2007-02-07 04:52:06 +0000

    GD 2.0.34 released.

--- gauche-gd/index.scm
+++ gauche-gd/index.scm
@@ -11,7 +11,7 @@
  (FreeType "http://www.freetype.org/")
  )
 
-(define *last-update* "Thu Jan 18 2007")
+(define *last-update* "Thu Feb 07 2007")
 (define *gauche-gd-version* (file->string "../VERSION"))
 (define *gauche-gd-tarball-basename* (string-append "Gauche-gd-" *gauche-gd-version* ".tgz"))
 (define *gauche-gd-tarball-size* (file-size (string-append "../../" *gauche-gd-tarball-basename*)))
@@ -35,6 +35,8 @@
 
 	 (html:h2 :style "border-bottom: 1px solid #bbbbbb;" (en/ja "News" "最新情報"))
 	 (html:ul
+	  (html:li "[2007-02-07] " (en/ja "Congratulation! GD 2.0.34 released."
+									  "GD 2.0.34 がリリースされました。"))
 	  (html:li "[2007-01-18] " (en/ja "It is confirmed that the current version 0.1.4 runs on Gauche 0.8.9."
 									  "Gauche 0.8.9 で現在のバージョン 0.1.4 が動作することを確認しました。"))
 	  (html:li "[2006-12-11] " (en/ja "Release 0.1.4. Some bugs fixed and some options added."
